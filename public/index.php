9cc7243ae3cf835c0f582787a319fff396216458
Alyssa Companioni <alyssa.companioni@gmail.com>
2026-02-24 15:21:07 -0500

    Updates index.php to get rid of extraneous comments

--- public/index.php
+++ public/index.php
@@ -5,19 +5,17 @@ require_once __DIR__ . '/../private/db_connection.php';
 echo "<h1>Capstone Project - Stage Plotter</h1>";
 echo "<p>PHP is working - yay!</p>";
 
-// $result = mysqli_query($connection, "SHOW TABLES");
 $result = $db->query("SHOW TABLES");
 
-if($result) {
+if ($result) {
   echo "<h2>Database Connection Successful</h2>";
 
   echo "<h3>Database Schema</h3>";
   echo "<p><a href='https://dbdiagram.io/d/StagePlotter-dev-6980d7d8bd82f5fce262143a' target='_blank'>View schema on dbdiagram.io</a></p>";
-  
+
   echo "<h3>Tables in database:</h3>";
   echo "<ul>";
-  // while($row = mysqli_fetch_array($result)) {
-  while($row = $result->fetch(PDO::FETCH_NUM)) {
+  while ($row = $result->fetch(PDO::FETCH_NUM)) {
     echo "<li>" . htmlspecialchars($row[0]) . "</li>";
   }
   echo "</ul>";
