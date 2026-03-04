83ff2f71b5f6682d34c34e10caf2becdcb66c802
Alyssa Companioni <alyssa.companioni@gmail.com>
2026-03-03 23:33:19 -0500

    Fixes formatting in header.php.

--- public/includes/header.php
+++ public/includes/header.php
@@ -18,35 +18,35 @@
     <ul>
       <?php if ($session->is_logged_in()) { ?>
 
-      <li><span class="nav-greeting">Hi, <?= htmlspecialchars($session->first_name) ?></span></li>
+        <li><span class="nav-greeting">Hi, <?= htmlspecialchars($session->first_name) ?></span></li>
         <li class="user-menu">
           <button class="user-menu-toggle" aria-expanded="false" aria-haspopup="true" aria-label="Open user menu">
             <img src="/assets/user.svg" alt="User menu" width="24" height="24" class="user-icon">
           </button>
-        
-        <ul class="user-dropdown" hidden>
 
-          <?php if ($session->has_role('super_admin')) { ?>
-            <li><a href="/index.php">Home</a></li>
-            <li><a href="/super_admin/dashboard.php">Super Admin Dashboard</a></li>
-            <li><a href="/super_admin/manage_users.php">Manage Users</a></li>
-            <li><a href="/manage_library.php">Manage Library</a></li>
-            <li><a href="/logout.php">Log Out</a></li>
+          <ul class="user-dropdown" hidden>
 
-          <?php } elseif ($session->has_role('admin')) { ?>
-            <li><a href="/index.php">Home</a></li>
-            <li><a href="/admin/dashboard.php">Admin Dashboard</a></li>
-            <li><a href="/admin/manage_members.php">Manage Members</a></li>
-            <li><a href="/manage_library.php">Manage Library</a></li>
-            <li><a href="/logout.php">Log Out</a></li>
+            <?php if ($session->has_role('super_admin')) { ?>
+              <li><a href="/index.php">Home</a></li>
+              <li><a href="/super_admin/dashboard.php">Super Admin Dashboard</a></li>
+              <li><a href="/super_admin/manage_users.php">Manage Users</a></li>
+              <li><a href="/manage_library.php">Manage Library</a></li>
+              <li><a href="/logout.php">Log Out</a></li>
 
-          <?php } elseif ($session->has_role('member')) { ?>
-            <li><a href="/index.php">Home</a></li>
-            <li><a href="/member/dashboard.php">Member Dashboard</a></li>
-            <li><a href="/logout.php">Log Out</a></li>
-          <?php } ?>
+            <?php } elseif ($session->has_role('admin')) { ?>
+              <li><a href="/index.php">Home</a></li>
+              <li><a href="/admin/dashboard.php">Admin Dashboard</a></li>
+              <li><a href="/admin/manage_members.php">Manage Members</a></li>
+              <li><a href="/manage_library.php">Manage Library</a></li>
+              <li><a href="/logout.php">Log Out</a></li>
 
-        </ul>
+            <?php } elseif ($session->has_role('member')) { ?>
+              <li><a href="/index.php">Home</a></li>
+              <li><a href="/member/dashboard.php">Member Dashboard</a></li>
+              <li><a href="/logout.php">Log Out</a></li>
+            <?php } ?>
+
+          </ul>
         </li>
 
       <?php } else { ?>
